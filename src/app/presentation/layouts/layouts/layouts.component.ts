3d074b2ebece237895cc1b8f5c3203568afe04f2
Ponniah96 <ponniah@DEV-LPT324.local>
2025-01-23 21:02:20 +0530

    EngageIQ Phase 1 Redesign Changes - Header sidenav Loader

--- src/app/presentation/layouts/layouts/layouts.component.ts
+++ src/app/presentation/layouts/layouts/layouts.component.ts
@@ -1,26 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
-import { SideNavComponent} from '../side-nav/side-nav.component';
+import { SideNavComponent } from '../side-nav/side-nav.component';
 import { LoginComponent } from '../../pages/login/login.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
-  imports: [SideNavComponent, RouterOutlet, LoginComponent],
+  imports: [
+    SideNavComponent,
+    RouterOutlet,
+    LoginComponent,
+    HeaderComponent,
+    CommonModule,
+  ],
   templateUrl: './layouts.component.html',
-  styleUrl: './layouts.component.scss'
+  styleUrl: './layouts.component.scss',
 })
 export class LayoutsComponent {
-  LoginScreen:boolean = true
-  constructor(private router:Router) {}
+  LoginScreen: boolean = true;
+  scroll: boolean = false;
+  constructor(private router: Router) {}
   ngOnInit() {
     this.router.events.subscribe(() => {
       const currentRoute = this.router.url;
       if (currentRoute === '/login') {
         this.LoginScreen = true;
       } else {
-        this.LoginScreen = false
+        this.LoginScreen = false;
       }
     });
+    window.addEventListener('scroll', this.onWindowScroll, true);
+  }
+
+  onWindowScroll(): void {
+    if (
+      document.documentElement.getElementsByClassName('content')[0].scrollTop >
+      0
+    ) {
+      console.log('Header Scrolls is working');
+      this.scroll = true;
+      document
+        .getElementsByClassName('content__header')[0]
+        .classList.add('scroll');
+    } else {
+      console.log('Header Scrolls is not working');
+      this.scroll = false;
+      document
+        .getElementsByClassName('content__header')[0]
+        .classList.remove('scroll');
+    }
   }
 }
